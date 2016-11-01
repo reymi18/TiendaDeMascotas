3c467870d053515fb12fcf3bd74a9feb06d092a7
Yancini <yancini1515@hotmail.com>
2016-11-01 05:27:13 -0500

    Prueba Unitaria

--- test/test.php
+++ test/test.php
@@ -1,28 +1,17 @@
-<?php
-
-class Prueba extends PHPUnit_Framework_TestCase
-{
-	
-
-public function testPrueba(){
-	
-
-	$this->assertEquals(true, true);
-}
-
-
-
-
-
-}
-
-
-
-
-
-
-
-
-
-
+<?php
+	include 'conexion.php';
+	class Prueba extends PHPUnit_Framework_TestCase
+	{
+		public function testPruebaUsuario(){
+			$conexion = new conexion();
+			$correo = 'yancini1515@hotmail.com';
+			$contra = 'gina';
+			$this->assertEquals('Ginnette', $conexion->verificarUsuario($correo,$contra));
+		}
+
+		/*public function testPruebaServicio(){
+			$servicio = 1;
+			$this->assertEquals(1, buscarServicio($servicio));
+		}*/
+	}
 ?>
